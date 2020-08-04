1032d75a990a91ab3ec27caf81ac3b597739d707
gabrieleBarlacchi <gabriele.barlacchi@waterview.it>
2020-08-04 16:53:30 +0200

    setup.py as original version

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ with open("README.rst", "rt") as f:
     DESCRIPTION = f.read()
 
 url = 'https://github.com/carlok/fss3wrap'
-url = 'https://github,com/barloccia/fss3wrap'
-version='0.1.21'
+version='0.1.20'
 
 setup(
   name = 'fss3wrap',
